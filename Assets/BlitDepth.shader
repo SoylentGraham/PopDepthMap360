896318dd1cc1535aeb60b7ab44acfe6197e4f493
Graham Reeves <graham@newchromantics.com>
2016-09-16 18:02:31 +0100

    initial depth capture for a camera

---
+++ Assets/BlitDepth.shader
@@ -0,0 +1,50 @@
+﻿Shader "NewChromantics/DepthBlit" {
+   Properties
+     {
+         DepthMax ("DepthMax", Range(0,0.3)) = 0.1
+     }
+     SubShader {
+Tags { "RenderType"="Opaque" }
+
+Pass{
+CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+#include "UnityCG.cginc"
+
+
+float DepthMax;
+sampler2D _CameraDepthTexture;
+
+struct v2f {
+   float4 pos : SV_POSITION;
+   float4 scrPos:TEXCOORD1;
+};
+
+//Vertex Shader
+v2f vert (appdata_base v){
+   v2f o;
+   o.pos = mul (UNITY_MATRIX_MVP, v.vertex);
+   o.scrPos=ComputeScreenPos(o.pos);
+   //for some reason, the y position of the depth texture comes out inverted
+   //	gr: not inverted
+    // o.scrPos.y = 1 - o.scrPos.y;
+   return o;
+}
+
+//Fragment Shader
+half4 frag (v2f i) : COLOR{
+
+  	float depthValue = Linear01Depth (tex2Dproj(_CameraDepthTexture, UNITY_PROJ_COORD(i.scrPos)).r);
+  	float DepthNorm = depthValue / DepthMax;
+
+  	DepthNorm = clamp( 0, 1, DepthNorm );
+  	DepthNorm = 1 - DepthNorm;
+
+  	return float4( DepthNorm, DepthNorm, DepthNorm, 1 );
+}
+ENDCG
+}
+}
+FallBack "Diffuse"
+}
---
+++ Assets/PopDepthCapture360.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering;
+
+
+public class PopDepthCapture360 : MonoBehaviour {
+
+	public Camera			SourceCamera;
+	public RenderTexture	EquirectDepth;
+	public RenderTexture	EquirectColour;
+	public Cubemap			DepthCubemap;
+	public Shader			DepthShader;
+	public Material			BlitDepth;
+
+	UnityEngine.Rendering.CommandBuffer	PostBlitDepthCommand;
+
+	void Update () {
+
+		if ( PostBlitDepthCommand == null )
+		{
+			PostBlitDepthCommand = new CommandBuffer ();
+			var Id = new RenderTargetIdentifier (EquirectDepth);
+			//int depthID = Shader.PropertyToID("_DepthCopyTexture"); 
+			PostBlitDepthCommand.Blit(BuiltinRenderTextureType.CurrentActive, Id, BlitDepth);
+			//cmd.SetGlobalTexture("_DepthBuffer", depthID);
+		}
+
+		RenderDepth (SourceCamera.transform.rotation, SourceCamera.transform.position, EquirectColour, EquirectDepth);
+
+	}
+
+
+	void RenderDepth(Quaternion Rotation,Vector3 Position,RenderTexture ColourTexture,RenderTexture DepthTexture)
+	{
+		var go = new GameObject ();
+		//	make a camera
+		var cam = go.AddComponent<Camera>();
+
+		cam.transform.position = Position;
+		cam.transform.rotation = Rotation;
+		cam.depthTextureMode = DepthTextureMode.Depth;
+		cam.fieldOfView = 90;
+		//cam.RenderToCubemap (CubemapTexture);
+		cam.targetTexture = ColourTexture;
+		cam.AddCommandBuffer (UnityEngine.Rendering.CameraEvent.AfterDepthTexture, PostBlitDepthCommand);
+		cam.Render ();
+		//Graphics.Blit (null, DepthTexture, BlitDepth);
+
+
+		Destroy (go);
+	}
+
+
+	void OnRenderImage (RenderTexture source, RenderTexture destination){
+
+		Debug.Log ("OnRenderImage");
+	
+		//Graphics.Blit(source,destination,mat);
+		//mat is the material which contains the shader
+		//we are passing the destination RenderTexture to
+	}
+}
